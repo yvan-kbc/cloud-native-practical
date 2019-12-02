39e29f6b6198fc817682d0660137c190a3f40531
Yvan Barbaix <yvan.barbaix@verz.kbc.be>
2019-12-02 10:54:03 +0100

    Lab01-commit

---
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
