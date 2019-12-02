85c6710625105d7fcab4beb2852123e25e3f62dc
Yvan Barbaix <yvan.barbaix@verz.kbc.be>
2019-12-02 12:45:13 +0100

    Cocktail API - with stubs

---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglistcocktail/CocktailController.java
@@ -0,0 +1,8 @@
+package com.ezgroceries.shoppinglist;
+/**
+ * @author Yvan Barbaix (u77439)
+ * @since release/201710
+ */
+public class CocktailController {
+
+}
---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglistcocktail/CocktailResource.java
@@ -0,0 +1,9 @@
+package com.ezgroceries.shoppinglistcocktail;
+
+/**
+ * @author Yvan Barbaix (u77439)
+ * @since release/201710
+ */
+public class CocktailResource {
+
+}
